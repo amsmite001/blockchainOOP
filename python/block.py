9fbb6bc28e1f0da16a1fa6ad3bfa2ad1e38b4da0
amsmite001 <smite.am@outlook.com>
2020-04-14 00:41:51 -0600

    progress

--- python/block.py
+++ python/block.py
@@ -13,4 +13,19 @@ class Block:
 
 class Blockchain:
     def __init__(self):
+        self.chain = []
+        self.setGenesisBlock(1,100)
+    def initBlock(self,previousHash):
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp' : time(),
+            'transaction': self.transaction
+            'previousHash': previousHash or self.hash(self.chain[-1])
+            }
         
+        self.chain.append(block)
+        return block
+    def setGenesisBlock(self):
+        genesis_Block = initBlock(
+    def calcHash(self):
+        return str(hashlib.sha256(self.index + self.prevoiusHash + self.timestamp + self.data))
