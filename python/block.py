a5ac04799aa780e9bd95b4ad8aa39d6cccaf669c
amsmite001 <smite.am@outlook.com>
2020-04-14 10:46:16 -0600

    fixed block.py

--- python/block.py
+++ python/block.py
@@ -1,31 +1,31 @@
 import hashlib
-import time
+import datetime
 
-class Block:
+class Block():
     def __init__(self, index, data, timestamp, prevhashS):
         self.index = index
         self.data = data
         self.timestamp = timestamp
         self.hashS = self.calcHash()
         self.prevhashS = prevhashS
-    def calcHash(self):
-        return str(hashlib.sha256(self.index + self.prevoiusHash + self.timestamp + self.data))
 
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.setGenesisBlock(1,100)
-    def initBlock(self,previousHash):
-        block = {
-            'index': len(self.chain)+1,
-            'timestamp' : time(),
-            'transaction': self.transaction
-            'previousHash': previousHash or self.hash(self.chain[-1])
-            }
-        
-        self.chain.append(block)
-        return block
-    def setGenesisBlock(self):
-        genesis_Block = initBlock(
     def calcHash(self):
-        return str(hashlib.sha256(self.index + self.prevoiusHash + self.timestamp + self.data))
+        return hashlib.sha256(self.index + self.prevoiusHash + self.timestamp + self.data).hexdigest()
+
+class Blockchain():
+    def __init__(self):
+        self.chain = [self.setGenesisBlock()]
+
+    def setGenesisBlock(self):
+        return Block(0,datetime.datetime.utcnow(), 'Genesis', '0')
+
+    def addBlock(self, data):
+        self.chain.append(Block(len(self.chain)+1,datetime.datetime.utcnow(), data, self.chain[len(self.chain)-1].hash))
+    
+    @property
+    def lenChain(self):
+        return len(self.chain)
+    
+    @property
+    def lastBlock(self):
+        return self.chain[-1]
